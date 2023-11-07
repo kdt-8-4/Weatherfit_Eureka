859738150e12b6317b4ea6cedf67e49d1aa70424
papicc45 <papicc45@naver.com>
2023-11-07 20:16:14 +0900

    initial project commit

---
+++ node_service/app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const Eureka = require('eureka-js-client').Eureka;
+
+const client = new Eureka({
+   // Eureka 서버의 설정
+   eureka: {
+      host: 'localhost',  // Eureka 서버의 주소
+      port: 8761,  // Eureka 서버의 포트
+      servicePath: '/eureka/apps/'
+   },
+   instance: {
+      app: 'nodejs-service',  // 서비스의 이름
+      hostName: 'localhost',  // 서비스의 호스트 이름
+      ipAddr: '127.0.0.1',  // 서비스의 IP 주소
+      port: {
+         '$': 8003,  // 서비스의 포트
+         '@enabled': 'true',
+      },
+      vipAddress: 'nodejs-service',  // 서비스의 VIP 주소
+      statusPageUrl: 'http://localhost:8003',  // 서비스의 상태 페이지 URL
+      healthCheckUrl: 'http://localhost:8003/health',  // 서비스의 헬스 체크 URL
+      dataCenterInfo: {
+         '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+         name: 'MyOwn',
+      },
+   },
+});
+app.get('/health', (req, res) => {
+   res.json({status: 'UP'});
+});
+
+app.get('/nodetest', (req, res)=> {
+   res.json({ check : 'check' });
+})
+client.start();  // Eureka 서버에 서비스를 등록
+app.listen(8003, ()=> {
+   console.log('localhost:8003');
+});
+module.exports = app;
