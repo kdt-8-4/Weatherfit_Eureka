427b13861ffbcbe660618efefcd88f5fb6d0b42c
papicc45 <papicc45@naver.com>
2023-11-09 17:14:09 +0900

    nodejs server enroll eureka

--- node_service/app.js
+++ node_service/app.js
@@ -21,21 +21,20 @@ const Eureka = require('eureka-js-client').Eureka;
 const client = new Eureka({
    // Eureka 서버의 설정
    eureka: {
-      host: 'localhost',  // Eureka 서버의 주소
+      host: '13.124.197.227:8761',  // Eureka 서버의 주소
       port: 8761,  // Eureka 서버의 포트
       servicePath: '/eureka/apps/'
    },
    instance: {
       app: 'nodejs-service',  // 서비스의 이름
-      hostName: 'localhost',  // 서비스의 호스트 이름
-      ipAddr: '127.0.0.1',  // 서비스의 IP 주소
+      ipAddr: '52.79.92.21',  // 서비스의 IP 주소
       port: {
          '$': 8003,  // 서비스의 포트
          '@enabled': 'true',
       },
       vipAddress: 'nodejs-service',  // 서비스의 VIP 주소
-      statusPageUrl: 'http://localhost:8003',  // 서비스의 상태 페이지 URL
-      healthCheckUrl: 'http://localhost:8003/health',  // 서비스의 헬스 체크 URL
+      statusPageUrl: 'http://52.79.92.21:8003',  // 서비스의 상태 페이지 URL
+      healthCheckUrl: 'http://52.79.92.21:8003/health',  // 서비스의 헬스 체크 URL
       dataCenterInfo: {
          '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
          name: 'MyOwn',
