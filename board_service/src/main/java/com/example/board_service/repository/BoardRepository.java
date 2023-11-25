771da7cc700703e710c653f2f1d4dcdf190dccce
papicc45 <papicc45@naver.com>
2023-11-25 09:55:46 +0900

    change repository

--- board_service/src/main/java/com/example/board_service/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.board_service.repository;
-
-import com.example.board_service.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
