096c51f01f75a039ead3e02b66a3515506c5f7b4
papicc45 <papicc45@naver.com>
2023-11-27 13:15:54 +0900

    feat : add token filter

--- api_gateway/src/main/java/com/example/api_gateway/filter/JwtTokenGlobalFilter.java
+++ api_gateway/src/main/java/com/example/api_gateway/filter/JwtTokenGlobalFilter.java
@@ -27,58 +27,58 @@ public class JwtTokenGlobalFilter implements GlobalFilter, Ordered {
         MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
         queryParams.forEach((key, value) -> System.out.println(key + ": " + value));
 
-//        String path = exchange.getRequest().getPath().toString();
-//        //category-service api들은 전부 토큰값 필요 없음
-//        if(path.startsWith("/category/")){
-//            return chain.filter(exchange);
-//        }
-//
-//        //토큰값이 필요하지 않은 user-service api
-//        if(path.equals("/user/signup") || path.equals("/user/signin") || path.equals("/api/login")) {
-//            return chain.filter(exchange);
-//        }
-//
-//        //토큰값이 필요하지 않은 board-service api
-//        if(path.equals("/board/list") || path.startsWith("/board/detail/") || path.equals("/board/search")) {
-//            return chain.filter(exchange);
-//        }
-//
-//        //토큰값이 필요하지 않은 comment-service api
-//        if(path.equals("/comment/comments")) {
-//            return chain.filter(exchange);
-//        }
-//
-//        //테스트용 api
-//        if(path.equals("/comment/jwtTest2")) {
-//            return chain.filter(exchange);
-//        }
-//
-//        String jwtToken = exchange.getRequest().getHeaders().getFirst("Authorization");
-//
-//        //jwt 토큰의 디코딩 값이 필요한 api들( /comment/jwtTest 는 테스트용 )
-//        if(path.equals("/comment/jwtTest") || path.equals("/board/write") || path.equals("/comment/write") || path.equals("/comment/reply")) {
-//            try {
-//                Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken.replace("Bearer ", "")).getBody();
-//
-//                exchange.getAttributes().put("decodedToken", claims.getSubject());
-//                exchange = exchange.mutate().request(exchange.getRequest().mutate().header("decodedToken", claims.toString()).build()).build();
-//
-//                return chain.filter(exchange);
-//            } catch (Exception e) {
-//                throw  new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Token");
-//            }
-//        }
-//
-//        //위 api 제외 나머지 api들
-//        if(jwtToken == null) {
-//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Jwt Token is missing");
-//        }
-//
-//        try {
-//            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(jwtToken.replace("Bearer ", ""));
-//        } catch (Exception e) {
-//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Token");
-//        }
+        String path = exchange.getRequest().getPath().toString();
+        //category-service api들은 전부 토큰값 필요 없음
+        if(path.startsWith("/category/")){
+            return chain.filter(exchange);
+        }
+
+        //토큰값이 필요하지 않은 user-service api
+        if(path.equals("/user/signup") || path.startsWith("/user/signin/") || path.equals("/api/login") || path.startsWith("/user/api/") || path.startsWith("/user/social")) {
+            return chain.filter(exchange);
+        }
+
+        //토큰값이 필요하지 않은 board-service api
+        if(path.equals("/board/list") || path.startsWith("/board/detail/") || path.equals("/board/search")) {
+            return chain.filter(exchange);
+        }
+
+        //토큰값이 필요하지 않은 comment-service api
+        if(path.equals("/comment/comments")) {
+            return chain.filter(exchange);
+        }
+
+        //테스트용 api
+        if(path.equals("/comment/jwtTest2")) {
+            return chain.filter(exchange);
+        }
+
+        String jwtToken = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        //jwt 토큰의 디코딩 값이 필요한 api들( /comment/jwtTest 는 테스트용 )
+        if(path.equals("/comment/jwtTest") || path.equals("/board/write") || path.equals("/comment/write") || path.equals("/comment/reply")) {
+            try {
+                Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken.replace("Bearer ", "")).getBody();
+
+                exchange.getAttributes().put("decodedToken", claims.getSubject());
+                exchange = exchange.mutate().request(exchange.getRequest().mutate().header("decodedToken", claims.toString()).build()).build();
+
+                return chain.filter(exchange);
+            } catch (Exception e) {
+                throw  new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Token");
+            }
+        }
+
+        //위 api 제외 나머지 api들
+        if(jwtToken == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Jwt Token is missing");
+        }
+
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(jwtToken.replace("Bearer ", ""));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Token");
+        }
 
         return chain.filter(exchange);
     }
